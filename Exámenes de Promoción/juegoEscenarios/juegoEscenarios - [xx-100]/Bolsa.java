039bdf5d07bb9b8ce69e994aa7063867f0fd4d15
Ernesto Monja <ernesto_09_@live.com.ar>
2026-02-16 21:08:12 -0300

    Update Bolsa.java

--- Exámenes de Promoción/juegoEscenarios/juegoEscenarios - [xx-100]/Bolsa.java
+++ Exámenes de Promoción/juegoEscenarios/juegoEscenarios - [xx-100]/Bolsa.java
@@ -1,16 +1,17 @@
-
 import java.util.TreeMap;
 
-public class Bolsa {
+public class Bolsa
+{
     private TreeMap<String, Elemento> elementos ;
     private final Integer capacidad;
 
     /**
-     * Constructor de Bolsa
+     * Constructor de Bolsa.
      *
-     * @param capacidad La capacidad de la bolsa
+     * @param capacidad     La capacidad de la bolsa.
      */
-    public Bolsa(Integer capacidad) {
+    public Bolsa(Integer capacidad)
+    {
         this.capacidad = capacidad;
         this.elementos = new TreeMap<>();
     }
@@ -18,11 +19,13 @@ public class Bolsa {
     /**
      * Anade un elemento a la bolsa.
      *
-     * @param e El elemento a anadir.
-     * @throws BolsaLlenaException Si la bolsa esta llena y no se puede anadir el elemento.
+     * @param  e                      El elemento a anadir.
+     * @throws BolsaLlenaException    Si la bolsa esta llena y no se puede anadir el elemento.
      */
-    public void addElemento (Elemento e) throws BolsaLlenaException {
-        if (capacidad == elementos.size()) {
+    public void addElemento(Elemento e) throws BolsaLlenaException
+    {
+        if (capacidad == elementos.size())
+        {
             throw new BolsaLlenaException("No hay espacio disponible en la bolsa");
         }
         elementos.put(e.getNombre(),e);
@@ -31,13 +34,15 @@ public class Bolsa {
     /**
      * Devuelve el elemento con el nombre suministrado.
      *
-     * @param nombre El nombre del elemento a buscar.
-     * @return El elemento con el nombre suministrado.
-     * @throws IllegalArgumentException Si no existe elemento con el nombre suministrado.
+     * @param  nombre                       El nombre del elemento a buscar.
+     * @return                              El elemento con el nombre suministrado.
+     * @throws IllegalArgumentException     Si no existe elemento con el nombre suministrado.
      */
-    public Elemento getElemento (String nombre) {
+    public Elemento getElemento(String nombre)
+    {
         Elemento e = elementos.get(nombre);
-        if (e == null) {
+        if(e == null)
+        {
             throw new IllegalArgumentException("No existe elemento con el nombre suministrado");
         }
         elementos.remove(nombre);
